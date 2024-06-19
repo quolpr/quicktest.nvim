80d76bc99cd213abc145c17368866456e53260e8
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-06-19 22:06:38 +0200

    Run test closest to cursor

--- lua/quicktest/adapters/meson/init.lua
+++ lua/quicktest/adapters/meson/init.lua
@@ -8,6 +8,17 @@ local function get_filename(path)
   return path:match("[^/]*.c$")
 end
 
+local function mysplit(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
 -- Assumes the test executable is named the same as the test except for the file ending
 -- e.g. test_name.c -> test_name
 local function get_test_exe_from_buffer(bufnr)
@@ -17,20 +28,28 @@ local function get_test_exe_from_buffer(bufnr)
   return filename
 end
 
---- Builds parameters for running tests based on buffer number and cursor position.
---- This function should be customized to extract necessary information from the buffer.
----@param bufnr integer
----@param cursor_pos integer[]
----@return any
-M.build_params = function(bufnr, cursor_pos)
-  -- You can get current function name to run based on bufnr and cursor_pos
-  -- Check hot it is done for golang at `lua/quicktest/adapters/golang`
-  print("HERPA DERPA")
+local function get_test_suite_and_name(line)
+  local parts = mysplit(line, "(")
+  local index = 1
+
+  if vim.startswith(parts[1], "ParameterizedTest") then
+    index = 2
+  end
+
+  parts = mysplit(parts[2], ",")
   return {
-    bufnr = bufnr,
-    cursor_pos = cursor_pos,
-    -- Add other parameters as needed
+    test_suite = string.gsub(parts[index], "%s+", ""),
+    test_name = string.gsub(parts[index + 1], "%s+", ""),
   }
+end
+
+local function get_nearest_test(bufnr, cursor_pos)
+  for pos = cursor_pos[1], 1, -1 do
+    local line = vim.api.nvim_buf_get_lines(bufnr, pos - 1, pos, true)[1]
+    if vim.startswith(line, "Test(") or vim.startswith(line, "ParameterizedTest(") then
+      return line
+    end
+  end
 end
 
 M.build_file_run_params = function(bufnr, cursor_pos)
@@ -44,7 +63,10 @@ end
 
 M.build_line_run_params = function(bufnr, cursor_pos)
   local test_exe = get_test_exe_from_buffer(bufnr)
+  local line = get_nearest_test(bufnr, cursor_pos)
+  local test = get_test_suite_and_name(line)
   return {
+    test = test,
     test_exe = test_exe,
     bufnr = bufnr,
     cursor_pos = cursor_pos,
@@ -67,6 +89,10 @@ M.run = function(params, send)
 
   if params.test_exe then
     table.insert(test_args, params.test_exe)
+  end
+
+  if params.test then
+    table.insert(test_args, "--test-args=--filter=" .. params.test.test_suite .. "/" .. params.test.test_name)
   end
 
   local job = Job:new({
