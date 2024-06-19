4844f979b761b5915b9aae327ec22cbba7087246
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-06-19 12:17:47 +0200

    Basic support for running all tests from the open buffer
    This is assuming the test executable is named after the test file, so:
    test_something.c -> exe == test_something
    
    So that the command line becomes
    meson test -C build test_something

--- lua/quicktest/adapters/meson/init.lua
+++ lua/quicktest/adapters/meson/init.lua
@@ -8,6 +8,15 @@ local function get_filename(path)
   return path:match("[^/]*.c$")
 end
 
+-- Assumes the test executable is named the same as the test except for the file ending
+-- e.g. test_name.c -> test_name
+local function get_test_exe_from_buffer(bufnr)
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  local filename = get_filename(bufname)
+  filename = string.gsub(filename, ".c", "", 1)
+  return filename
+end
+
 --- Builds parameters for running tests based on buffer number and cursor position.
 --- This function should be customized to extract necessary information from the buffer.
 ---@param bufnr integer
@@ -25,16 +34,18 @@ M.build_params = function(bufnr, cursor_pos)
 end
 
 M.build_file_run_params = function(bufnr, cursor_pos)
-  print("funcs-bufnre", bufnr)
-
+  local test_exe = get_test_exe_from_buffer(bufnr)
   return {
+    test_exe = test_exe,
     bufnr = bufnr,
     cursor_pos = cursor_pos,
   }
 end
 
 M.build_line_run_params = function(bufnr, cursor_pos)
+  local test_exe = get_test_exe_from_buffer(bufnr)
   return {
+    test_exe = test_exe,
     bufnr = bufnr,
     cursor_pos = cursor_pos,
   }
@@ -52,9 +63,15 @@ end
 ---@param send fun(data: any)
 ---@return integer
 M.run = function(params, send)
+  local test_args = { "test", "-C", "build" }
+
+  if params.test_exe then
+    table.insert(test_args, params.test_exe)
+  end
+
   local job = Job:new({
     command = "meson",
-    args = { "test", "-C", "build" }, -- Modify based on how your test command needs to be structured
+    args = test_args, -- Modify based on how your test command needs to be structured
     on_stdout = function(_, data)
       send({ type = "stdout", output = data })
     end,
