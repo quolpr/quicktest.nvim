312b1493cb78d6a39d8066568e1593aa757b58d9
Sergey Popov <quolpr@gmail.com>
2024-06-21 19:00:00 +0300

    Remove reload command

--- plugin/quicktest.lua
+++ plugin/quicktest.lua
@@ -1,17 +1,17 @@
-vim.api.nvim_create_user_command("C", function()
-  require("lazy.core.loader").reload("quicktest.nvim")
-
-  -- require("quicktest").open_win("popup")
-  -- local module = require("quicktest.module")
-  --
-  -- module.run(require("quicktest.adapters.golang"), {
-  --   func_names = { "TestSum" },
-  --   sub_func_names = {},
-  --   cwd = "/Users/quolpr/.config/nvim/go_test",
-  --   module = "./abc",
-  --   bufnr = 0,
-  -- })
-end, {})
+-- vim.api.nvim_create_user_command("C", function()
+--   require("lazy.core.loader").reload("quicktest.nvim")
+--
+--   -- require("quicktest").open_win("popup")
+--   -- local module = require("quicktest.module")
+--   --
+--   -- module.run(require("quicktest.adapters.golang"), {
+--   --   func_names = { "TestSum" },
+--   --   sub_func_names = {},
+--   --   cwd = "/Users/quolpr/.config/nvim/go_test",
+--   --   module = "./abc",
+--   --   bufnr = 0,
+--   -- })
+-- end, {})
 
 -- vim.api.nvim_create_user_command("Go", function()
 --   local module = require("quicktest.module")
